9ac014112a3371586c3b38e9ad4e8fd986599b96
Evelyn Quintero Ochoa <evelynquintero@berkeley.edu>
2023-02-06 14:46:13 -0800

    summative proj 1

---
+++ Summative-1-QuinteroOchoa-Evelyn/src/main/java/com/company/randomizer/models/Quote.java
@@ -0,0 +1,37 @@
+package com.company.randomizer.models;
+
+public class Quote {
+    private int id;
+    private String author;
+    private String quote;
+
+    public Quote(String quote, String author, int id) {
+        this.quote = quote;
+        this.author = author;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
